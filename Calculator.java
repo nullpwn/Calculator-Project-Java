d2082ba3eb615db15929dd7d3bfe8910fa470d32
nullpwn <alwinpaul4040@gmail.com>
2022-02-24 14:47:57 +0530

    Added Action Listener to Numbers

--- Calculator.java
+++ Calculator.java
@@ -9,13 +9,27 @@ import javax.swing.SwingConstants;
 
 public class Calculator implements ActionListener {
     JFrame jf;
+    JLabel displayLabel;
+    JButton sevenButton;
+    JButton eightButton;
+    JButton nineButton;
+    JButton fourButton;
+    JButton fiveButton;
+    JButton sixButton;
+    JButton oneButton;
+    JButton twoButton;
+    JButton threeButton;
+    JButton zeroButton;
+
+
+
     public Calculator() {
         jf = new JFrame("Calculator");
         jf.setLayout(null);
         jf.setSize(600, 600);
         jf.setLocation(300, 150);
 
-        JLabel displayLabel = new JLabel("OK");
+        displayLabel = new JLabel();
         displayLabel.setBounds(30, 50, 540, 40);
         displayLabel.setBackground(Color.DARK_GRAY);
         displayLabel.setOpaque(true);
@@ -23,51 +37,59 @@ public class Calculator implements ActionListener {
         displayLabel.setForeground(Color.white);
         jf.add(displayLabel);
 
-        JButton sevenButton = new JButton("7"); // ROW 1
+        sevenButton = new JButton("7"); // ROW 1
         sevenButton.setBounds(30, 130, 80, 80);
         sevenButton.setFont(new Font("Arial",Font.PLAIN,30));
         jf.add(sevenButton);
         sevenButton.addActionListener(this);
 
-        JButton eightButton = new JButton("8");
+        eightButton = new JButton("8");
         eightButton.setBounds(130, 130, 80, 80);
         eightButton.setFont(new Font("Arial",Font.PLAIN,30));
         jf.add(eightButton);
+        eightButton.addActionListener(this);
 
-        JButton nineButton = new JButton("9");
+        nineButton = new JButton("9");
         nineButton.setBounds(230, 130, 80, 80);
         nineButton.setFont(new Font("Arial",Font.PLAIN,30));
         jf.add(nineButton);
+        nineButton.addActionListener(this);
 
-        JButton fourButton = new JButton("4"); //ROW 2
+        fourButton = new JButton("4"); //ROW 2
         fourButton.setBounds(30, 230, 80, 80);
         fourButton.setFont(new Font("Arial",Font.PLAIN,30));
         jf.add(fourButton);
+        fourButton.addActionListener(this);
 
-        JButton fiveButton = new JButton("5");
+        fiveButton = new JButton("5");
         fiveButton.setBounds(130, 230, 80, 80);
         fiveButton.setFont(new Font("Arial",Font.PLAIN,30));
         jf.add(fiveButton);
+        fiveButton.addActionListener(this);
 
-        JButton sixButton = new JButton("6");
+        sixButton = new JButton("6");
         sixButton.setBounds(230, 230, 80, 80);
         sixButton.setFont(new Font("Arial",Font.PLAIN,30));
         jf.add(sixButton);
+        sixButton.addActionListener(this);
 
-        JButton oneButton = new JButton("1"); //ROW 3
+        oneButton = new JButton("1"); //ROW 3
         oneButton.setBounds(30, 330, 80, 80);
         oneButton.setFont(new Font("Arial",Font.PLAIN,30));
         jf.add(oneButton);
+        oneButton.addActionListener(this);
 
-        JButton twoButton = new JButton("2");
+        twoButton = new JButton("2");
         twoButton.setBounds(130, 330, 80, 80);
         twoButton.setFont(new Font("Arial",Font.PLAIN,30));
         jf.add(twoButton);
+        twoButton.addActionListener(this);
 
-        JButton threeButton = new JButton("3");
+        threeButton = new JButton("3");
         threeButton.setBounds(230, 330, 80, 80);
         threeButton.setFont(new Font("Arial",Font.PLAIN,30));
         jf.add(threeButton);
+        threeButton.addActionListener(this);
 
 
         JButton dotButton = new JButton("."); //ROW 4
@@ -75,10 +97,11 @@ public class Calculator implements ActionListener {
         dotButton.setFont(new Font("Arial",Font.PLAIN,30));
         jf.add(dotButton);
 
-        JButton zeroButton = new JButton("0"); 
+        zeroButton = new JButton("0"); 
         zeroButton.setBounds(130, 430, 80, 80);
         zeroButton.setFont(new Font("Arial",Font.PLAIN,30));
         jf.add(zeroButton);
+        zeroButton.addActionListener(this);
 
         JButton equalButton = new JButton("=");
         equalButton.setBounds(230, 430, 80, 80);
@@ -122,7 +145,27 @@ public class Calculator implements ActionListener {
     
     @Override
     public void actionPerformed(ActionEvent e) {
-       jf.getContentPane().setBackground(Color.blue);
+        if(e.getSource() == sevenButton){
+       displayLabel.setText("7");
+        } else if(e.getSource() == eightButton){
+            displayLabel.setText("8");
+        }else if(e.getSource() == nineButton){
+            displayLabel.setText("9");
+        }else if(e.getSource() == fourButton){
+            displayLabel.setText("4");
+        }else if(e.getSource() == fiveButton){
+            displayLabel.setText("5");
+        }else if(e.getSource() == sixButton){
+            displayLabel.setText("6");
+        }else if(e.getSource() == oneButton){
+            displayLabel.setText("1");
+        }else if(e.getSource() == twoButton){
+            displayLabel.setText("2");
+        }else if(e.getSource() == threeButton){
+            displayLabel.setText("3");
+        }else if(e.getSource() == zeroButton){
+            displayLabel.setText("0");
+        }
         
     }
 
